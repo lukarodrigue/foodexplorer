4fa13d8317324bddd76f4e6a859f33a5cd28d67b
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-03-13 00:57:26 -0300

    ✨ feat: add component Section

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -2,13 +2,17 @@ import { Container } from "./styles";
 
 import { Header } from '../../components/Header';
 import { Textarea } from "../../components/Textarea";
+import { Section } from "../../components/Section";
 
 export function New() {
     return (
         <Container>
-            <Header isAdmin />
 
-            <Textarea placeholder="Fale brevemente sobre o prato, seus ingredientes e composição" />
+            <Header isAdmin />
+            <Section title={"Descrição"}>
+                <Textarea placeholder="Fale brevemente sobre o prato, seus ingredientes e composição" />
+            </Section>
+
         </Container>
     );
 }
