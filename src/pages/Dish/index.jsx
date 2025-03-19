9e384a36e89fe6000a0fc61dd78134c744656459
luka <lukarodrigues25@gmail.com>
2025-03-19 14:44:01 -0400

    ✨ feat: add component ButtonText

--- src/pages/Dish/index.jsx
+++ src/pages/Dish/index.jsx
@@ -1,17 +1,27 @@
+import { RxCaretLeft } from "react-icons/rx";
+
 import { Container } from "./styles";
 
 import { Header } from "../../components/Header";
 import { Tag } from "../../components/Tag";
 import { Footer } from "../../components/Footer";
+import { ButtonText } from "../../components/ButtonText";
+
 
 export function Dish() {
     return (
         <Container>
 
-            <Header />
-                <Tag title="alface"/>
-                <Tag title="cebola"/>
+            <ButtonText>
+                <RxCaretLeft />
+                Voltar
+            </ButtonText>
 
+
+
+            <Header />
+            <Tag title="alface" />
+            <Tag title="cebola" />
             <Footer />
 
         </Container>
