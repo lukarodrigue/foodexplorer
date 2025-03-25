21517ffbd773640b25f44206e150f5856fed40be
luka <lukarodrigues25@gmail.com>
2025-03-24 23:28:42 -0300

    ✨ feat: add routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { ThemeProvider } from "styled-components";
-import GlobalStyles from "./styles/global"
-import theme from "./styles/theme"
+import GlobalStyles from "./styles/global";
+import theme from "./styles/theme";
 
-import { SignUp   } from "./pages/SignUp";
+import { Routes } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles>
-        <SignUp />
+        <Routes />
       </GlobalStyles>
     </ThemeProvider>
   </React.StrictMode>
