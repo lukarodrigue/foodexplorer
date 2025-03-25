c1fb22ad2c09992d5862e4a343e3de066f7de938
luka <lukarodrigues25@gmail.com>
2025-03-24 23:33:03 -0300

    ✨ feat: finish desktop SignIn page

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,15 +1,36 @@
-import { Container } from "./styles";
+import { Link } from "react-router-dom";
+
+import { Container, Form, Brand } from "./styles";
+import { Section } from "../../components/Section";
 
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 
+import brand from "../../assets/brand.svg";
 
 export function SignIn() {
-    return (
-        <Container>
-            <Input placeholder="Exemplo: exemplo@exemplo.com.br" />
-            <Input placeholder="No mínimo 6 caracteres" />
-            <Button title={Entrar} />
-        </Container>
-    );
+  return (
+    <Container>
+      <Button title="Entrar" />
+      <Brand>
+        <img src={brand} alt="Logo" />
+      </Brand>
+
+      <Form>
+        <h2>Faça seu login</h2>
+
+        <Section title="Email">
+          <Input placeholder="Exemplo: exemplo@exemplo.com.br" type="text" />
+        </Section>
+
+        <Section title="Senha">
+          <Input placeholder="No mínimo 6 caracteres" type="password" />
+        </Section>
+
+        <Button title="Entrar" />
+
+        <Link to="/register">Criar uma conta</Link>
+      </Form>
+    </Container>
+  );
 }
