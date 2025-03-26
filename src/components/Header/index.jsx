c5ebb4ee6383e1ff11ff5a2a56eba21c98f113c0
luka <lukarodrigues25@gmail.com>
2025-03-26 10:15:41 -0400

    ✨ feat: improve component Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,38 +1,55 @@
-import { FiSearch, FiLogOut } from "react-icons/fi";
+import { FiMenu, FiLogOut } from "react-icons/fi";
+import { MdClose } from "react-icons/md";
+import { useMediaQuery } from "react-responsive";
 
-import { Container, Brand, Search, Logout } from "./styles";
+import { Container, Menu, Brand, Logout } from "./styles";
 
-import { Input } from "../../components/Input";
+import { Search } from "../../components/Search";
 import { Button } from "../../components/Button";
 
 import brand from "../../assets/brand.svg";
 import brandAdmin from "../../assets/brand-admin.svg";
+import brandMobile from "../../assets/brand-mobile.svg";
 
-export function Header({ isAdmin }) {
-  const logo = isAdmin ? brandAdmin : brand;
+export function Header({ isAdmin, isMenuOpen, setIsMenuOpen }) {
+  const isDesktop = useMediaQuery({ minWidth: 1024 });
+
+  const logo = isAdmin ? (isDesktop ? brandAdmin : brandMobile) : brand;
 
   return (
     <Container>
-      <Brand>
-        <img src={logo} alt="Logo" />
-      </Brand>
-
-      <Search>
-        <Input
-          placeholder="Busque por pratos ou ingredientes"
-          icon={FiSearch}
-        />
-      </Search>
-
-      {isAdmin ? (
-        <Button title="Novo prato" />
-      ) : (
-        <Button title="Pedidos" isCustomer orderCount={0} />
+      {!isDesktop && (
+        <Menu>
+          {!isMenuOpen ?
+            <FiMenu className="fi-menu-icon" onClick={() => setIsMenuOpen(true)} /> :
+            <>
+              <MdClose size={18} onClick={() => setIsMenuOpen(false)} />
+              <span>Menu</span>
+            </>
+          }
+        </Menu>
       )}
 
-      <Logout>
-        <FiLogOut />
-      </Logout>
+      {(isDesktop || !isMenuOpen) && (
+        <>
+          <Brand>
+            <img src={logo} alt="Logo" />
+          </Brand>
+
+          {isDesktop && <Search />}
+
+          {isAdmin ?
+            (isDesktop && <Button title="Novo prato" />) :
+            <Button title={isDesktop ? "Pedidos" : undefined} isCustomer orderCount={0} />
+          }
+
+          {isDesktop &&
+            <Logout>
+              <FiLogOut size={32} />
+            </Logout>
+          }
+        </>
+      )}
     </Container>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,67 +2,75 @@ import styled from "styled-components";
 
 export const Container = styled.header`
    grid-area: header;
- 
-   height: 96px;
+   height: 114px;
    width: 100%;
-   padding: 24px 123px;
- 
+   padding: 56px 28px 24px;
    display: flex;
-   gap: 32px;
-   
    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_300};
-   
-   > button:first-of-type {
-     max-width: 216px;
-   }
- `;
+   button:last-child {
+    width: auto;
+    background: none; 
+  }
 
-export const Brand = styled.div`
-   display: flex;
-   align-items: center;
+   @media (min-width: 1024px) {
+     height: 96px;
+     padding: 24px 123px;
  
-   > img {
-     width: 197px;
-     height: 39px;
-   }
- `;
-
-export const Search = styled.div`
-   display: flex;
-   padding: 12px 14px;
-   flex-grow: 1;
-   border-radius: 5px;
- 
-   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_500};
-
-   &:focus-within {
-     border: 1px solid ${({ theme }) => theme.COLORS.WHITE};
-   }
-   
-   svg {
-     color: ${({ theme }) => theme.COLORS.GRAY_100};
-   }
- 
-   input {
-     max-width: 282px;
-
-     &:focus {
-       border: none;
+     justify-content: space-between;
+     gap: 32px;
+     
+     > button:first-of-type {
+       max-width: 216px;
      }
    }
  `;
 
-export const Logout = styled.button`
-   border: none;
-   background: none;
- 
-   display: flex;
-   align-items: center;
-   
-   color: ${({ theme }) => theme.COLORS.WHITE};
-   
-   > svg {
-     width: 32px;
-     height: 32px;
+export const Menu = styled.button`
+    border: none;
+    background: none;
+    display: flex;
+    align-items: center;
+    gap: 16px;
+
+    color: ${({ theme }) => theme.COLORS.WHITE};
+  
+
+    .fi-menu-icon {
+     width: 24px;
+     height: 18px;
    }
- `;
+ 
+   span {
+     font-size: 21.16px;
+   }
+  `;
+
+export const Brand = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  > img {
+     width: 160.61px;
+     height: 26px;
+   }
+   @media (min-width: 1024px) {
+    width: auto;
+  }
+  > img {
+      width: 197px;
+      height: 39px;
+    }
+  `;
+
+export const Logout = styled.button`
+  border: none;
+  background: none;
+
+  display: flex;
+  align-items: center;
+
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  
+  `;
