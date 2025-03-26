8aa6e84599c6b71f68e4b5b4769768e8e6039137
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-03-26 07:20:39 -0300

    ✨ feat: improve component NumberPicker

--- src/components/NumberPicker/styles.js
+++ src/components/NumberPicker/styles.js
@@ -1,18 +1,30 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 14px;
-
-  color: ${({ theme }) => theme.COLORS.GRAY_200};
-
-  svg {
-     cursor: pointer;
+   display: flex;
+   align-items: center;
+   gap: 14px;
+ 
+   color: ${({ theme }) => theme.COLORS.GRAY_200};
+ 
+   button {
+     border: none;
+     background: none;
+ 
+     color: ${({ theme }) => theme.COLORS.GRAY_200};
      transition: filter 0.2s;
  
      &:hover {
        filter: brightness(0.9);
      }
    }
-`;
+ 
+   @media (min-width: 1024px) {
+     svg,
+     span {
+       font-weight: 700;
+       font-size: 20px;
+       line-height: 160%;
+     }
+   }
+ `;
