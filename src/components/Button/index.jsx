7e8bd93f53a6de75e7107eab9018cae310d02ab1
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-03-26 07:15:03 -0300

    ✨ feat: improve component Button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,13 +1,22 @@
 import { TbReceipt } from "react-icons/tb";
 
+import { useMediaQuery } from 'react-responsive';
+
 import { Container } from "./styles";
 
 export function Button({ title, loading = false, isCustomer, ...rest }) {
+
+  const isDesktop = useMediaQuery({ minWidth: 1024 })
+
   return (
     <Container type="button" disable={loading} {...rest}>
+
+
       {isCustomer && <TbReceipt size={32} />}
       {loading ? "Carregando..." : title}
-      {isCustomer && <span>({rest.orderCount})</span>}
+      {isCustomer && <span>{isDesktop ? `(${rest.orderCount})` : rest.orderCount}</span>}
+
+
     </Container>
   );
 }
