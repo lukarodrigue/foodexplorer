94e33df491740a93a3ca94fcd2b16e9434ada39a
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-03-26 00:00:27 -0300

    ✨ feat: finish desktop Dish page

--- src/pages/Dish/index.jsx
+++ src/pages/Dish/index.jsx
@@ -1,29 +1,58 @@
 import { RxCaretLeft } from "react-icons/rx";
 
-import { Container } from "./styles";
+import { Container, Content } from "./styles";
 
-import { Header } from "../../components/Header";
-import { Tag } from "../../components/Tag";
-import { Footer } from "../../components/Footer";
+import { Header } from '../../components/Header';
 import { ButtonText } from "../../components/ButtonText";
+import { Tag } from '../../components/Tag';
+import { NumberPicker } from "../../components/NumberPicker";
+import { Button } from "../../components/Button";
+import { Footer } from '../../components/Footer';
 
-
-export function Dish() {
+export function Dish({ isAdmin }) {
     return (
         <Container>
 
-            <ButtonText>
-                <RxCaretLeft />
-                Voltar
-            </ButtonText>
+            {isAdmin ? <Header isAdmin /> : <Header />}
 
+            <main>
+                <header>
+                    <ButtonText>
+                        <RxCaretLeft />
+                        voltar
+                    </ButtonText>
+                </header>
 
+                <Content>
+                    <img src="../../src/assets/salada-ravanello.png" alt="Salada Ravanello" />
 
-            <Header />
-            <Tag title="alface" />
-            <Tag title="cebola" />
+                    <div>
+                        <h1>Salada Ravanello</h1>
+                        <p>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim. O pão naan dá um toque especial.</p>
+
+                        <section>
+                            <Tag title="alface" />
+                            <Tag title="cebola" />
+                            <Tag title="pão naan" />
+                            <Tag title="pepino" />
+                            <Tag title="rabanete" />
+                            <Tag title="tomate" />
+                        </section>
+
+                        <div className="buttons">
+                            {isAdmin ?
+                                <Button title="Editar prato" className="edit" /> :
+                                <>
+                                    <NumberPicker />
+                                    <Button title="incluir ∙ R$ 25,00" className="include" />
+                                </>
+                            }
+                        </div>
+                    </div>
+                </Content>
+            </main>
+
             <Footer />
-
         </Container>
     );
-}
+};
