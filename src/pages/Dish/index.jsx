67af624e7f112cce00a51936ceb7c10165e1a95e
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-03-28 00:33:36 -0300

    ✨ feat: finish mobile Dish page

--- src/pages/Dish/index.jsx
+++ src/pages/Dish/index.jsx
@@ -2,6 +2,10 @@ import { RxCaretLeft } from "react-icons/rx";
 
 import { Container, Content } from "./styles";
 
+import { useMediaQuery } from 'react-responsive';
+import { useState } from 'react'
+
+import { Menu } from "../../components/Menu";
 import { Header } from '../../components/Header';
 import { ButtonText } from "../../components/ButtonText";
 import { Tag } from '../../components/Tag';
@@ -10,49 +14,63 @@ import { Button } from "../../components/Button";
 import { Footer } from '../../components/Footer';
 
 export function Dish({ isAdmin }) {
+
+    const isDesktop = useMediaQuery({ minWidth: 1024 });
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
         <Container>
 
-            {isAdmin ? <Header isAdmin /> : <Header />}
+            {!isDesktop &&
+                <Menu isAdmin={isAdmin} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+            }
+
+            <Header isAdmin={isAdmin} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
 
             <main>
-                <header>
-                    <ButtonText>
-                        <RxCaretLeft />
-                        voltar
-                    </ButtonText>
-                </header>
+                <div>
+                    <header>
+                        <ButtonText>
+                            <RxCaretLeft />
+                            voltar
+                        </ButtonText>
+                    </header>
 
-                <Content>
-                    <img src="../../src/assets/salada-ravanello.png" alt="Salada Ravanello" />
+                    <Content>
+                        <img src="../../src/assets/salada-ravanello.png" alt="Salada Ravanello" />
 
-                    <div>
-                        <h1>Salada Ravanello</h1>
-                        <p>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim. O pão naan dá um toque especial.</p>
+                        <div>
+                            <h1>Salada Ravanello</h1>
+                            <p>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim. O pão naan dá um toque especial.</p>
 
-                        <section>
-                            <Tag title="alface" />
-                            <Tag title="cebola" />
-                            <Tag title="pão naan" />
-                            <Tag title="pepino" />
-                            <Tag title="rabanete" />
-                            <Tag title="tomate" />
-                        </section>
+                            <section>
+                                <Tag title="alface" />
+                                <Tag title="cebola" />
+                                <Tag title="pão naan" />
+                                <Tag title="pepino" />
+                                <Tag title="rabanete" />
+                                <Tag title="tomate" />
+                            </section>
 
-                        <div className="buttons">
-                            {isAdmin ?
-                                <Button title="Editar prato" className="edit" /> :
-                                <>
-                                    <NumberPicker />
-                                    <Button title="incluir ∙ R$ 25,00" className="include" />
-                                </>
-                            }
+                            <div className="buttons">
+                                {isAdmin ?
+                                    <Button title="Editar prato" className="edit" /> :
+                                    <>
+                                        <NumberPicker />
+                                        <Button
+                                            title={isDesktop ? "incluir ∙ R$ 25,00" : "pedir ∙ R$ 25,00"}
+                                            className="include"
+                                            isCustomer={!isDesktop}
+                                        />
+                                    </>
+                                }
+                            </div>
                         </div>
-                    </div>
-                </Content>
-            </main>
+                    </Content>
+                </div >
+            </main >
 
             <Footer />
-        </Container>
+        </Container >
     );
 };
