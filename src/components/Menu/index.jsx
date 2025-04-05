55ebf21efce79cd24c10843e58c62d8e3b33962f
luka rodrigues goncalves <lukarodrigues25@gmail.com>
2025-04-05 00:57:55 -0300

    ✨ feat: handle new and signout

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,24 +1,41 @@
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/auth';
+
 import { Container } from "./styles";
 
 import { Header } from '../../components/Header';
 import { Search } from "../Search";
 import { ButtonText } from "../ButtonText";
 
-export function Menu({ isAdmin, isMenuOpen, setIsMenuOpen, setSearch }) {
+export function Menu({ isAdmin, isMenuOpen, setIsMenuOpen, setSearch, isDisabled }) {
+    const { signOut } = useAuth();
+    const navigate = useNavigate();
+
+    function handleNew() {
+        navigate("/new");
+    }
+
+    function handleSignOut() {
+        navigate("/");
+        signOut();
+    }
     return (
         <Container isMenuOpen={isMenuOpen}>
             <Header isAdmin={isAdmin} isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
 
             <main>
-                <Search setSearch={setSearch} />
 
-                {isAdmin &&
-                    <ButtonText>
+                <Search isDisabled={isDisabled} setSearch={setSearch} />
+
+
+                {isAdmin ? (
+                    <ButtonText onClick={handleNew}>
                         Novo prato
                     </ButtonText>
-                }
 
-                <ButtonText>
+                ) : null}
+
+                <ButtonText onClick={handleSignOut}>
                     Sair
                 </ButtonText>
             </main>
